23a9024f8bcd24be85661161fb51ba43d8b797d4
KJIeonaTpa <kseniia.naumova44@gmail.com>
2024-06-23 14:52:19 +0300

    Add files via upload

--- tests/api/test_fixtures.py
+++ tests/api/test_fixtures.py
@@ -1,10 +1,5 @@
 import pytest
 
-@pytest.mark.change
-def test_remove_name(user):
-    user.name = ''
-    assert user.name == ''
-
 @pytest.mark.check
 def test_chenge_name(user):
     assert user.name == 'Sergii'
